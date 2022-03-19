b7af2f6f3f7dd89742999a2f974f1242c2002a40
Andrei Grini <Andrei.Grini@avanssoma.no>
2022-03-19 08:44:40 +0200

    new migrations

---
+++ migrations/heroes/1647293117998_insert_into_public_heroes/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."heroes" WHERE "id" = '7860a1b7-8583-4395-8245-a1c07ca5af58';
---
+++ migrations/heroes/1647293117998_insert_into_public_heroes/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."heroes"("id", "name", "gender", "species", "photo", "rating") VALUES (E'7860a1b7-8583-4395-8245-a1c07ca5af58', E'Batman', E'male', E'human', E'batman.png', 99);
